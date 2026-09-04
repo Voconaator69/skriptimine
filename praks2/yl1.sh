64b6d9e58289fdd230e1569e2ba71a14547246f3
Henri Antson <henri.antson@voco.ee>
2026-09-04 10:38:23 +0300

    lõik kasutaja tervitamise skripti

---
+++ praks2/yl1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#ülemine rida on koorik(käivitamisinterpritaator)
+#mitte unustada teha chmod 755 skriptile
+#Skripti eesmärk on luua script mis väljastab arvuti kasutajale tervituse
+echo "Tere, $USER!"
